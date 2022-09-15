2da10da64c01001f96faa1dbd9e987728a75c165
Leonardo Pellegrino <leonardo.pellegrino@avancoinfo.com.br>
2022-09-15 01:34:42 -0300

    fix(adress): adicionando promise de retorno

--- src/core/services/adress.service.ts
+++ src/core/services/adress.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { Iadress } from '../interfaces/adress.model';
+import { firstValueFrom } from 'rxjs';
+import { AxiosResponse } from 'axios';
 
 @Injectable()
 export class AdressService {
   constructor(private readonly _httpService: HttpService) {}
 
-  getAdressInfo(zipcode: string | number) {
+  getAdressInfo(zipcode: string | number): Promise<AxiosResponse<Iadress>> {
     const url = `https://viacep.com.br/ws/${zipcode}/json/`;
-    return this._httpService.get<Iadress>(url);
+    return firstValueFrom(this._httpService.get<Iadress>(url));
   }
 }
